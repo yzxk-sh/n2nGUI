dd0cfc79064bc1b4fd0b3b3d9139937c39279349
烟渚萧客 <kyle_dar@163.com>
2019-01-24 15:08:25 +0800

    删除了一些无用的代码和注释。

---
+++ n2nGUI/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace n2nGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.supernodePortEdit = new System.Windows.Forms.TextBox();
+            this.supernodeAddEdit = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.group = new System.Windows.Forms.TextBox();
+            this.ipSet = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.OK = new System.Windows.Forms.Button();
+            this.Abort = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.supernodePortEdit);
+            this.groupBox1.Controls.Add(this.supernodeAddEdit);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.groupBox1.Location = new System.Drawing.Point(12, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 87);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Supernode";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.groupBox2.Location = new System.Drawing.Point(-1, 101);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 87);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Supernode";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(76, 56);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(97, 21);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(76, 23);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(97, 21);
+            this.textBox4.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "端口";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "节点地址";
+            // 
+            // supernodePortEdit
+            // 
+            this.supernodePortEdit.Location = new System.Drawing.Point(76, 56);
+            this.supernodePortEdit.Name = "supernodePortEdit";
+            this.supernodePortEdit.Size = new System.Drawing.Size(97, 21);
+            this.supernodePortEdit.TabIndex = 4;
+            // 
+            // supernodeAddEdit
+            // 
+            this.supernodeAddEdit.Location = new System.Drawing.Point(76, 23);
+            this.supernodeAddEdit.Name = "supernodeAddEdit";
+            this.supernodeAddEdit.Size = new System.Drawing.Size(97, 21);
+            this.supernodeAddEdit.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(19, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "端口";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(19, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "节点地址";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "程序设定";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox3.Controls.Add(this.password);
+            this.groupBox3.Controls.Add(this.label10);
+            this.groupBox3.Controls.Add(this.group);
+            this.groupBox3.Controls.Add(this.ipSet);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.groupBox3.Location = new System.Drawing.Point(12, 137);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 122);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "本机设置";
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(76, 89);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(97, 21);
+            this.password.TabIndex = 7;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label10.Location = new System.Drawing.Point(19, 92);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(29, 12);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "密码";
+            // 
+            // group
+            // 
+            this.group.Location = new System.Drawing.Point(76, 56);
+            this.group.Name = "group";
+            this.group.Size = new System.Drawing.Size(97, 21);
+            this.group.TabIndex = 4;
+            // 
+            // ipSet
+            // 
+            this.ipSet.Location = new System.Drawing.Point(76, 23);
+            this.ipSet.Name = "ipSet";
+            this.ipSet.Size = new System.Drawing.Size(97, 21);
+            this.ipSet.TabIndex = 2;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label8.Location = new System.Drawing.Point(19, 59);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 12);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "组";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label9.Location = new System.Drawing.Point(19, 26);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(41, 12);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "IP设定";
+            // 
+            // OK
+            // 
+            this.OK.Location = new System.Drawing.Point(42, 270);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(40, 24);
+            this.OK.TabIndex = 7;
+            this.OK.Text = "确定";
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // Abort
+            // 
+            this.Abort.Location = new System.Drawing.Point(136, 270);
+            this.Abort.Name = "Abort";
+            this.Abort.Size = new System.Drawing.Size(40, 24);
+            this.Abort.TabIndex = 8;
+            this.Abort.Text = "中止";
+            this.Abort.UseVisualStyleBackColor = true;
+            this.Abort.Click += new System.EventHandler(this.Abort_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "n2nGUI";
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_DoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ExitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 26);
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.ExitToolStripMenuItem.Text = "退出";
+            this.ExitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 301);
+            this.Controls.Add(this.Abort);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "n2nGUI";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox supernodePortEdit;
+        private System.Windows.Forms.TextBox supernodeAddEdit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox group;
+        private System.Windows.Forms.TextBox ipSet;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button OK;
+        private System.Windows.Forms.Button Abort;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
+    }
+}
+
---
+++ n2nGUI/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+/*
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+*/
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+
+namespace n2nGUI
+{
+    public partial class Form1 : Form
+    {
+        int processID;
+        public Form1()
+        {
+            int x = (SystemInformation.WorkingArea.Width - this.Size.Width) / 2;
+            int y = (SystemInformation.WorkingArea.Height - this.Size.Height) / 2;
+            InitializeComponent();
+            //this.StartPosition = FormStartPosition.Manual;
+            //this.Location = (Point)new Size(x, y);
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            string serverAdd, serverPort, edgeAdd, edgeGroup, edgePasswd;
+            serverAdd = supernodeAddEdit.Text;
+            serverPort = supernodePortEdit.Text;
+            edgeAdd = ipSet.Text;
+            edgeGroup = group.Text;
+            edgePasswd = password.Text;
+
+            string para = " -a " + edgeAdd + " -c " + edgeGroup + " -k " + edgePasswd + " -l " + serverAdd + ":" + serverPort;
+            string path = System.Environment.CurrentDirectory;
+            //          Process myProcess = Process.Start("D:/kyle/Downloads/n2n/win32/DotNet/Release/edge.exe", para);        
+            //Process myProcess = Process.Start(path + "/edge.exe", para);
+            //processID = myProcess.Id;
+            //Console.WriteLine(path);
+
+            ProcessStartInfo Info = new ProcessStartInfo
+            {
+                FileName = "edge.exe",
+                WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
+                Arguments = para,
+                WorkingDirectory = path
+            };
+
+
+            System.Diagnostics.Process Proc;
+            try
+            { // //启动外部程序 // 
+                Proc = System.Diagnostics.Process.Start(Info);
+                processID = Proc.Id;
+            }
+            catch (System.ComponentModel.Win32Exception exc)
+            {
+                Console.WriteLine("系统找不到指定的程序文件。/r{0}", exc);
+
+            }
+        }
+
+        private void Abort_Click(object sender, EventArgs e)
+        {
+            Process myProcessA = Process.GetProcessById(processID);     // 通过ID关联进程
+            myProcessA.Kill();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //取消关闭窗口
+            e.Cancel = true;
+            //最小化主窗口
+            this.WindowState = FormWindowState.Minimized;
+            //不在系统任务栏显示主窗口图标
+            this.ShowInTaskbar = false;
+            this.notifyIcon.Visible = true;
+
+        }
+
+        private void notifyIcon_DoubleClick(object sender, MouseEventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+                notifyIcon.Visible = true;
+                this.ShowInTaskbar = true;
+            }
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (processID != 0)
+            {
+                Abort_Click(null, null);
+            }
+            Application.Exit();
+        }
+    }
+}
